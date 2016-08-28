afaf362aed8a17e5c52ec03fcbdb9b2f0ea7a56d
Sean Kochel <seankochel@gmail.com>
2016-08-28 14:31:21 -0400

    trying to deploy to heroku for first time

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://localhost/shopping-list-dev') ||
+                       (process.env.NODE_ENV === 'test' ?
+                       	'mongodb://localhost/shopping-list' :
+                       	'mongodb://<ragnar_p>:<oreos272727>ds017896.mlab.com:17896/mongo-test');
 exports.PORT = process.env.PORT || 8080;
